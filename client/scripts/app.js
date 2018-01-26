d53b17dc5c00f39998df347cb80527c212974038
Scott Barclay <scottdbarclay@gmail.com>
2018-01-26 11:13:07 -0800

    Add function stubs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,35 @@
-// YOUR CODE HERE:
+// stub out functions
+var app = {};
+
+var init = function() {
+
+};
+
+var send = function() {
+
+};
+
+var fetch = function() {
+
+};
+
+var clearMessages = function() {
+
+};
+
+var renderMessage = function() {
+
+};
+
+var renderRoom = function() {
+
+};
+
+var handleUsernameClick = function () {
+
+};
+
+var handleSubmit = function() {
+
+};
+
